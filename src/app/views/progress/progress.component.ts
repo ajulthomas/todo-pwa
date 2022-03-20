5bbdf8c1c0d858a073e30543c7f95ce846b03619
ajulthomas <ajul.thomas@outlook.com>
2022-03-20 22:39:20 +0530

    adds drag and drop functionality to progress view

--- src/app/views/progress/progress.component.ts
+++ src/app/views/progress/progress.component.ts
@@ -1,35 +1,25 @@
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
-import { Task } from 'src/app/interfaces/project.interface';
+import { Task, TaskStatus } from 'src/app/interfaces/project.interface';
+import { TaskService } from 'src/app/services/task/task.service';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.scss'],
 })
 export class ProgressComponent implements OnInit {
-  tasks: Task[] = [
-    {
-      name: 'UI/UX Design',
-      status: 'COMPLETED',
-      project: 'ABC Sales App',
-    },
-    {
-      name: 'Design Approval',
-      status: 'IN_PROGRESS',
-      project: 'ABC Sales App',
-    },
-    {
-      name: 'Resource Gathering',
-      status: 'TODO',
-      project: 'ABC Sales App',
-    },
-    {
-      name: 'UI Development - Phase 1',
-      status: 'TODO',
-      project: 'ABC Sales App',
-    },
-  ];
+  tasks: Task[] = [];
 
-  constructor() {}
+  constructor(private taskService: TaskService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
+
+  onDrop(e: CdkDragDrop<any>, taskStatus: TaskStatus) {
+    let droppedTask: Task = e.item.data;
+    droppedTask.status = taskStatus;
+    this.taskService.updateTasks([droppedTask]);
+    this.tasks = this.taskService.getTasks();
+  }
 }
